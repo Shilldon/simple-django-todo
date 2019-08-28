bbbe9a246c225f286c3d701c201ad930d7f4c5ee
Ubuntu <ubuntu@ip-172-31-88-19.ec2.internal>
2019-08-28 20:05:40 +0000

    Updated ALLOWED_HOSTS

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '-o^$*!7b+u3xvzcu@6x6@aq5e9e-(34o_&6514m45efcn86@vo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['e33e15562f10479db8eb019b976078ea.vfs.cloud9.us-east-1.amazonaws.com']
+ALLOWED_HOSTS = ['e33e15562f10479db8eb019b976078ea.vfs.cloud9.us-east-1.amazonaws.com','simple-django-todo1.herokuapp.com']
 
 
 # Application definition
